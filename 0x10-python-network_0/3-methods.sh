d8cc692fb60ebbd47ad7d0c8978a6daff45f6942
Nugwaojo <magiroyale21@gmail.com>
2024-04-02 02:02:38 -0700

    Corrected

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i Allow | awk '{print $2}'
+# This script sends a request to the URL passed as argument and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i Allow | awk -F ': ' '{print $2}'
