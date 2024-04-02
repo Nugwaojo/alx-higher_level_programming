15360f56aaa6503c04358a509bf56e90a498fdb6
Nugwaojo <magiroyale21@gmail.com>
2024-04-02 02:53:01 -0700

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module fetches the status from a given URL using urllib package.
+"""
+import urllib.request
+
+
+def fetch_status():
+
+    """
+    Fetches the status from a URL and displays the body response.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode('utf-8')))
+
+
+if __name__ == "__main__":
+
+    fetch_status()
